a98f5e75db80de240d1ff6d59ae36705e8b5946e
dlagksrb_00 <dlagksrb0430@naver.com>
2024-01-16 10:10:52 +0900

    0124 api수정

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,6 +3,7 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +15,14 @@ import java.util.List;
 @RestController
 public class UserController {
 
+    private  final UserService userService;
     //private  final List<User> users = new ArrayList<>(); * 이문장은 메모리에 저장한다 -> 서버를 온오프시 데이터 없어짐
 private final JdbcTemplate jdbcTemplate;
 
 public UserController(JdbcTemplate jdbcTemplate){
+
     this.jdbcTemplate = jdbcTemplate;
+    this.userService = new UserService(jdbcTemplate);
 }
     @PostMapping("/user") // Post / user
     public void saveUser(@RequestBody UserCreateRequest request) {
@@ -47,7 +51,7 @@ public UserController(JdbcTemplate jdbcTemplate){
     }
     @PutMapping("/user")
     public void updateUser(@RequestBody UserUpdateRequest request){
-
+        userService.updateUser(jdbcTemplate, request);
     }
 
     @DeleteMapping("/user")
